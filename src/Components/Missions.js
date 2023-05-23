ee45aca628f64e7d7b2a41271bd8dfbf873e767a
Tom Daniel <tomudoh258@gmail.com>
2023-05-23 10:32:37 +0100

    Fetch data from missions api

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useEffect } from 'react'
+import { fetchMissionsData } from '../redux/missions/missionsSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Missions = () => (
-  <div>Missions</div>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+
+  useEffect(() => {
+    dispatch(fetchMissionsData());
+  }, [dispatch])
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {missions.map((mission) => (
+          <tr key={mission.id}>
+            <td>{mission.mission_name}</td>
+            <td>{mission.description}</td>
+            <td>Not a member</td>
+            <td><button type='button'>Join Mission</button></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Missions;
--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,15 +1,54 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+
+
+  const initialState = {
+    missions: [],
+    status: 'idle',
+    error: null,
+  };
+
+
+export const fetchMissionsData = createAsyncThunk('missions/getData', async () => {
+  try {
+    const response = await axios.get('https://api.spacexdata.com/v3/missions');
+    const chosenData = response.data.map((mission) => ({
+      id: mission.id,
+      mission_name: mission.name,
+      description: mission.description
+    }));
+    return chosenData;
+  }catch (error) {
+    throw new Error('Failed to fetch missions data')
+  }
+});
 
 const missionsSlice = createSlice({
   name: 'missions',
-  initialState: [],
+  initialState,
   reducers: {
     addMission: (state, action) => {
-      state.push(action.payload);
+      state.missions.push(action.payload);
     },
-    removeMission: (state, action) => state.filter((mission) => mission.id !== action.payload),
+    removeMission: (state, action) => state.missions.filter((mission) => mission.id !== action.payload),
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissionsData.pending, (state) => {
+        state.status = 'Loading';
+      })
+      .addCase(fetchMissionsData.fulfilled, (state, { payload }) => {
+        state.status = 'success';
+        state.missions = payload;
+      })
+      .addCase(fetchMissionsData.rejected, (state, action) => {
+        state.status = 'error';
+        state.error = action.error.message;
+      })
+  }
 });
+  
 
 export const { addMission, removeMission } = missionsSlice.actions;
 
