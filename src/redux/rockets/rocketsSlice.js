fb4719dfc800302bbdf984553d64173708c19ed1
Collins Bawa <collinskwamebawa@yahoo.com>
2023-05-23 01:10:39 +0000

    Fix linters error

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
@@ -10,7 +9,7 @@ const initialState = {
 
 export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get('www.fakeapi.com/');
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch rockets');
@@ -27,21 +26,21 @@ const rocketsSlice = createSlice({
     removeRockets: (state, action) => {
       const rocketsIndex = state.rockets.findIndex((rocket) => rocket.id === action.payload);
       if (rocketsIndex !== -1) {
-        state.rockets.splice(bookIndex, 1);
+        state.rockets.splice(rocketsIndex, 1);
       }
     },
   },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchBooks.pending, (state) => {
+      .addCase(fetchRockets.pending, (state) => {
         state.status = 'loading';
       })
-      .addCase(fetchBooks.fulfilled, (state, { payload }) => {
+      .addCase(fetchRockets.fulfilled, (state, { payload }) => {
         state.status = 'succeeded';
         state.books = Object.entries(payload)
-          .flatMap(([key, value]) => value.map((book) => ({ ...book, item_id: key })));
+          .flatMap(([id, value]) => value.map((rocket) => ({ ...rocket, id })));
       })
-      .addCase(fetchBooks.rejected, (state, action) => {
+      .addCase(fetchRockets.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       });
